5f1562d6d8c7363bc0a253dbf4aa89031bdd021d
denast <denast@proton.me>
2026-04-14 19:34:44 +0400

    added a markdown renderer (internal)

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,6 +1,36 @@
 -- Extra or experimental plugins
--- hello world! hello world!
 return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "norg", "org", "codecompanion" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      heading = {
+        icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
+      },
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 1,
+      },
+      checkbox = {
+        unchecked = { icon = "󰄱 " },
+        checked = { icon = "󰱒 " },
+        custom = {
+          todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo" },
+        },
+      },
+      bullet = {
+        icons = { "●", "○", "◆", "◇" },
+      },
+    },
+    keys = {
+      { "<leader>um", "<cmd>RenderMarkdown toggle<cr>", desc = "Toggle Markdown Render" },
+    },
+  },
   { -- funny plugin to create an accumulating snowfall effect over your code
     "marcussimonsen/let-it-snow.nvim",
     cmd = "LetItSnow", -- Wait with loading until command is run
